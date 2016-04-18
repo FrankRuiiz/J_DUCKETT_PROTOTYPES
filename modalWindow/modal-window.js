a7eff02bdad913a35c02a07cc1ee7603cc20946a
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-04-18 11:32:02 -0700

    updated readme - Frank

---
+++ modalWindow/modal-window.js
@@ -0,0 +1,10 @@
+var modal = (function(){
+    var $window = $(window);
+    var $modal = $('<div class="modal">');
+    var $content = $('<div class="modal-content">');
+    var $close = $('<button role="button" class="modal-close">close</button>');
+
+    $modal.append($content, $close);
+
+    
+}());
