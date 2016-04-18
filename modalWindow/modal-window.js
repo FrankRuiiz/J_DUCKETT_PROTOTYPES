3982ab7a86c85d15d798f0a4aab61da6ac48fa58
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-04-18 12:27:38 -0700

    basic modal functionality complete - Frank

--- modalWindow/modal-window.js
+++ modalWindow/modal-window.js
@@ -6,5 +6,39 @@ var modal = (function(){
 
     $modal.append($content, $close);
 
-    
+    $close.on('click', function(e) {
+        e.preventDefault();
+        modal.close();
+    });
+
+    return ({
+        center: function() {
+           // Calculate the distance from top and left of window to center the modal
+           var top =  Math.max($window.height() - $modal.outerHeight(), 0) / 2;
+           var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
+
+           $modal.css({
+               top: top + $window.scrollTop(),
+               left: left + $window.scrollLeft()
+           });
+       },
+
+        open: function(settings) {
+            $content.empty().append(settings.content);
+
+            $modal.css({
+                width: settings.width || 'auto',
+                height: settings.height || 'auto'
+            }).appendTo('body');
+
+            modal.center();
+            $(window).on('resize', modal.center);
+        },
+
+        close: function() {
+            $content.empty();
+            $modal.detach();
+            $(window).off('resize', modal.center);
+        }
+    });
 }());
