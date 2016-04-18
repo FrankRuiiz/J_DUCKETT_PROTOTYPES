41d8f684e1689a96bb7ba135b26c66fb4469fd04
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-04-18 10:59:42 -0700

    added click handler for opening modal - Frank

---
+++ modalWindow/modal-init.js
@@ -0,0 +1,13 @@
+
+(function() {
+    
+    var $content = $('#share-options').detach();  // Remove modal from the page
+    
+    $('#share').on('click', function() {          // Click handler to open modal
+        modal.open({
+            content: $content,
+            width: 340,
+            height: 300
+        });
+    });
+}());
