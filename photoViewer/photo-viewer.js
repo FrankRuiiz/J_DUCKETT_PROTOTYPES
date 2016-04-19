177caa2bfd4ab2e605fe6ce1cf1d4f7551127b9f
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-04-19 15:53:16 -0700

    added thumbnail functionality

--- photoViewer/photo-viewer.js
+++ photoViewer/photo-viewer.js
@@ -1,3 +1,21 @@
-/**
- * Created by FrankyR on 4/19/2016.
- */
+var request;
+var $current;
+var casche = {};
+var $frame = $('#photo-viewer');
+var $thumbs = $('.thumb');
+
+function crossfade($img) {
+    
+    if ($current) {
+        $current.stop().fadeOut('slow');
+    }
+    
+    $img.css({
+        marginLeft: -$img.width() / 2,
+        marginTop: -$img.height() / 2
+    });
+    
+    $img.stop().fadeTo('slow, 1');
+    
+    $current = $img;
+}
