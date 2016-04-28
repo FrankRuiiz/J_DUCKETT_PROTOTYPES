8826d15d1a955e68e3a81d561a99f052b27408bf
Frank Ruiz <frank.ruiz.limon@gmail.com>
2016-04-28 12:44:28 -0700

    table sort proto complete - Frank

--- sortingTable/script.js
+++ sortingTable/script.js
@@ -31,3 +31,39 @@ var compare = {             // Declare a compare object
         return a - b;       // return a - b
     }
 };
+
+$('.sortable').each(function() {
+    var $table = $(this);
+    var $tbody = $table.find('tbody');
+    var $controls = $table.find('th');
+    var rows = $tbody.find('tr').toArray();  // store array containing rows
+
+    $controls.on('click', function() {
+        var $header = $(this);              // get the header
+        var order = $header.data('sort');  //  get value of data-sort attribute
+        var column;                        // declare var column
+
+        // if selected item has ascending or descending class, reverse contents
+        if ($header.is('.ascending') || $header.is('.descending')) {
+            $header.toggleClass('ascending descending');  // toggle to other class
+            $tbody.append(rows.reverse());                // reverse the array
+        }
+        else {
+            $header.addClass('ascending');
+
+            // remove asc or desc from all other headers
+            $header.siblings().removeClass('ascending descending');
+
+            if (compare.hasOwnProperty(order)) {        // if compare object has the method (order)
+                column = $controls.index(this);         // search for column's index number
+
+                rows.sort(function(a, b) {      // cal sort on rows array
+                    a = $(a).find('td').eq(column).text(); // get the text of column in row a
+                    b = $(b).find('td').eq(column).text(); // get text of column in row b
+                    return compare[order](a, b);
+                });
+                $tbody.append(rows);
+            }
+        }
+    });
+});
